60def3bcef31642f7a96310ff468e5b8c1ca14d9
LS4Tonio <toniolesniper@gmail.com>
2017-07-10 16:57:47 +0200

    Added game object model

--- BatailleNavale/App.fs
+++ BatailleNavale/App.fs
@@ -4,6 +4,7 @@ open Suave                      // always open suave
 open Suave.Web                  // always open suave
 open BatailleNavale.Rest
 open BatailleNavale.db.users
+open BatailleNavale.db.games
 open Rules
 open Suave.Successful
 
@@ -18,11 +19,20 @@ let main argv =
         UpdateById = DbUsers.updateUserById
         IsExists = DbUsers.isUserExists
     }
+    let gameWebPart = rest "games" {
+        GetAll = DbGames.getAll
+        Create = DbGames.createGame
+        Update = DbGames.updateGame
+        Delete = DbGames.deleteGame
+        GetById = DbGames.getById
+        UpdateById = DbGames.updateGameById
+        IsExists = DbGames.isGameExists
+    }
 
     let defaultWebPart = OK "Hello World"
 
-    printsomething 1
+    //printsomething 1
 
-    startWebServer defaultConfig (choose [userWebPart; defaultWebPart])
+    startWebServer defaultConfig (choose [userWebPart; gameWebPart; defaultWebPart])
     //main must end with int
     0
